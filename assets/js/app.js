dfe62bca3a8c66ddd9bedefaf048f5b34d5bc7da
Andrew Berndt <abrnst@gmail.com>
2018-05-22 19:44:51 -0500

    a

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 var number = 30;
 var intervalId;   
+var upVotes = 0;
  
 
 var config = {
@@ -103,15 +104,16 @@ $("#submit-btn").on("click", function(){
         });
         run();
     });
-
+var clicked = $(this);
 //Tyler's upvote function
     $("body").on("click", ".rest-card", function() {
+
         event.preventDefault();
         console.log(this);
      
         var restID = $(this).attr("data-restID");
         console.log("Restaurant ID: " + restID);
-        var upVotes = 3;
+       
      
         database.ref(restID).on("value", function(snapshot) {
             // upVotes = snapshot.val().Upvotes;
@@ -125,9 +127,68 @@ $("#submit-btn").on("click", function(){
         };
         database.ref(restID).set(newRest)
         console.log(newRest);
+       
+       
+        reLoad();
      
      });
+     function reLoad(){
+        console.log(this);
+        var index1 = Math.floor(Math.random()* restArray.length)
+        var index2 = Math.floor(Math.random()* restArray.length)
+        
+        do{
+            index2 = Math.floor(Math.random()* restArray.length)
+        }while (index1==index2)
 
+        console.log(restArray[index1].restaurant.name,restArray[index2].restaurant.name)
+
+        var restaurants={
+            rest1:{
+                Name: restArray[index1].restaurant.name,
+                Img: restArray[index1].restaurant.featured_image,
+                Location: restArray[index1].restaurant.location.address,
+                Cost : restArray[index1].restaurant.average_cost_for_two,
+                Cousines : restArray[index1].restaurant.cuisines,
+                Rating : restArray[index1].restaurant.user_rating.aggregate_rating,
+                RestID : restArray[index1].restaurant.R.res_id,
+            },
+            rest2:{
+                Name: restArray[index2].restaurant.name,
+                Img: restArray[index2].restaurant.featured_image,
+                Location: restArray[index2].restaurant.location.address,
+                Cost : restArray[index2].restaurant.average_cost_for_two,
+                Cousines : restArray[index2].restaurant.cuisines,
+                Rating : restArray[index2].restaurant.user_rating.aggregate_rating,
+                RestID : restArray[index2].restaurant.R.res_id,
+            },
+        }
+
+        function appendRest1(){
+            $("#rest1").empty()
+            $("#rest1").append("<img src='"+clicked.restaurants.rest1.Img+ "'>")
+            $("#rest1").append("<h1 class='rest-name'>"+clicked..restaurants.rest1.Name+ "</h1>")
+            $("#rest1").append("<p class='rest-location'>"+"<span class='title'>Address: </span>"+clicked.restaurants.rest1.Location+ "</p>")
+            $("#rest1").append("<p class='rest-cousines'>"+"<span class='title'>Cousines: </span>"+clicked.restaurants.rest1.Cousines+ "</p>")
+            $("#rest1").append("<p class='rest-cost'>"+"<span class='title'>Average cost for two: </span>"+clicked..restaurants.rest1.Cost+ "</p>")
+            $("#rest1").append("<p class='rest-rating'>"+"<span class='title'>Rating: </span>"+clicked.restaurants.rest1.Rating+ "</p>")
+            $("#rest1").attr("data-restID", clicked.restaurants.rest1.RestID)
+        }
+        
+        function appendRest2(){
+            $("#rest2").empty()
+            $("#rest2").append("<img src='"+restaurants.rest2.Img+ "'>")
+            $("#rest2").append("<h1 class='rest-name'>"+restaurants.rest2.Name+ "</h1>")
+            $("#rest2").append("<p class='rest-location'>"+"<span class='title'>Address: </span>"+restaurants.rest2.Location+ "</p>")
+            $("#rest2").append("<p class='rest-cousines'>"+"<span class='title'>Cousines: </span>"+restaurants.rest2.Cousines+ "</p>")
+            $("#rest2").append("<p class='rest-cost'>"+"<span class='title'>Average cost for two: </span>"+restaurants.rest2.Cost+ "</p>")
+            $("#rest2").append("<p class='rest-rating'>"+"<span class='title'>Rating: </span>"+restaurants.rest2.Rating+ "</p>")
+            $("#rest2").attr("data-restID", restaurants.rest1.RestID)
+        }
+
+        appendRest1();
+        appendRest2();
+}
 
      function run() {
         clearInterval(intervalId);
@@ -142,7 +203,6 @@ $("#submit-btn").on("click", function(){
         num.html("<h2>" + number + "</h2>")
         $("#timer").html(num);
         if (number === 0) {
-        alert("Time Up!");
           stop();
           
         }
