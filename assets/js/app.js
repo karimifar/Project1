21c7e95014fda4001b134916250e5a4f1baaaeec
gugucode <meiyuechang@gmail.com>
2018-05-28 22:57:03 -0500

    add map function

--- assets/js/app.js
+++ assets/js/app.js
@@ -282,3 +282,14 @@ $("body").on("click", ".rest-card div", function() {
 //feature the winner restaurant
 //+ highlight the restaurants the user clicked on 
 //retry button
+
+// map function
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiZ3VndWNvZGUiLCJhIjoiY2pocjU1Z3R2MWMwcjM3cHZnZDhqa3NyYyJ9.6qeZqaN1FcIHVZqSut1hgw'
+}).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
